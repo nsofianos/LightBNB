b213907df89f12a4dd26c8c171e8d4d76e50f52f
Nik Sofianos <nsofianos@hotmail.com>
2020-11-25 03:58:30 +0000

    add more data to tables, add queries

---
+++ 1_queries/04_most_visited_cities.sql
@@ -0,0 +1,6 @@
+SELECT properties.city, count(reservations) AS total_reservations
+FROM properties
+JOIN reservations ON property_id = properties.id
+GROUP BY city
+ORDER BY total_reservations DESC
+LIMIT 10;
